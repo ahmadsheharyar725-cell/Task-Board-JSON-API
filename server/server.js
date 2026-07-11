96849e0045fb0c539ffe04dc49fa3e7893432aa5
Sheharyar <ahmadsheharyar725@gmail.com>
2026-07-11 23:48:11 +0500

    Fix server deployment

--- server/server.js
+++ server/server.js
@@ -6,19 +6,21 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Database files
 const teamFile = path.join(__dirname, "../db/team.json");
 const tasksFile = path.join(__dirname, "../db/tasks.json");
 
 app.use(cors());
 app.use(express.json());
 
+// Get Team + Tasks
 app.get("/", (req, res) => {
   try {
-    const teamdata = fs.readFileSync(teamFile, "utf8");
-    const tasksdata = fs.readFileSync(tasksFile, "utf8");
+    const teamData = fs.readFileSync(teamFile, "utf8");
+    const tasksData = fs.readFileSync(tasksFile, "utf8");
 
-    const team = JSON.parse(teamdata);
-    const tasks = JSON.parse(tasksdata);
+    const team = JSON.parse(teamData);
+    const tasks = JSON.parse(tasksData);
 
     res.json({
       team,
@@ -34,10 +36,11 @@ app.get("/", (req, res) => {
   }
 });
 
+// Add Task
 app.post("/tasks", (req, res) => {
   try {
-    const taskdata = fs.readFileSync(tasksFile, "utf8");
-    const tasks = JSON.parse(taskdata);
+    const taskData = fs.readFileSync(tasksFile, "utf8");
+    const tasks = JSON.parse(taskData);
 
     const newTask = req.body;
 
@@ -47,58 +50,66 @@ app.post("/tasks", (req, res) => {
 
     res.status(201).json(newTask);
   } catch (err) {
-    console.error("Error saving task:", err);
-    res.status(500).json({ error: "Failed to save task!" });
+    console.error(err);
+
+    res.status(500).json({
+      error: "Failed to save task!",
+    });
   }
 });
 
+// Delete Task
 app.delete("/tasks/:id", (req, res) => {
   try {
-    const taskdata = fs.readFileSync(tasksFile, "utf8");
-    const tasks = JSON.parse(taskdata);
+    const taskData = fs.readFileSync(tasksFile, "utf8");
+    const tasks = JSON.parse(taskData);
 
     const taskId = parseInt(req.params.id);
 
-    const updatedTask = tasks.filter((task) => task.id !== taskId);
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
 
-    fs.writeFileSync(tasksFile, JSON.stringify(updatedTask, null, 4));
+    fs.writeFileSync(tasksFile, JSON.stringify(updatedTasks, null, 4));
 
-    res.status(200).json({
+    res.json({
       message: "Task deleted successfully!",
     });
   } catch (err) {
-    console.error("Error deleting task:", err);
+    console.error(err);
+
     res.status(500).json({
       error: "Failed to delete task!",
     });
   }
 });
 
+// Update Task
 app.patch("/tasks/:id", (req, res) => {
   try {
-    const taskdata = fs.readFileSync(tasksFile, "utf8");
-    const tasks = JSON.parse(taskdata);
+    const taskData = fs.readFileSync(tasksFile, "utf8");
+    const tasks = JSON.parse(taskData);
 
     const taskId = parseInt(req.params.id);
     const updates = req.body;
 
-    const updatedTask = tasks.map((task) =>
+    const updatedTasks = tasks.map((task) =>
       task.id === taskId ? { ...task, ...updates } : task
     );
 
-    fs.writeFileSync(tasksFile, JSON.stringify(updatedTask, null, 4));
+    fs.writeFileSync(tasksFile, JSON.stringify(updatedTasks, null, 4));
 
-    res.status(200).json({
+    res.json({
       message: "Task updated successfully!",
     });
   } catch (err) {
-    console.error("Error updating task:", err);
+    console.error(err);
+
     res.status(500).json({
       error: "Failed to update task!",
     });
   }
 });
 
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
